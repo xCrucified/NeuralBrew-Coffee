cd9294b27986685451e1ee6ca5575c29c3b8e507
Your Name <you@example.com>
2025-09-27 22:40:40 +0200

    refactor layout and styles; remove unused main component, update HTML structure, and enhance CSS for better layout management

--- tauri-app/src/common/layout.js
+++ tauri-app/src/common/layout.js
@@ -11,33 +11,32 @@ async function injectLayout(
       document.body.prepend(d);
       return d;
     })();
-  const _main =
-    document.getElementById(mainId) ||
-    (() => {
-      const m = document.createElement("main");
-      m.id = mainId;
-      document.body.insertBefore(m, document.getElementById(secondId));
-      return m;
-    })();
-  const _footer =
-    document.getElementById(footerId) ||
-    (() => {
-      const f = document.createElement("footer");
-      f.id = footerId;
-      document.body.appendChild(f);
-      return f;
-    })();
+  // const _main =
+  //   document.getElementById(mainId) ||
+  //   (() => {
+  //     const d = document.createElement("main");
+  //     d.id = mainId;
+  //     document.body.insertBefore(d, document.getElementById(secondId));
+  //     return d;
+  //   })();
+  // const _footer =
+  //   document.getElementById(footerId) ||
+  //   (() => {
+  //     const d = document.createElement("footer");
+  //     d.id = footerId;
+  //     document.body.appendChild(d);
+  //     return d;
+  //   })();
 
   const headerRes = await fetch("./components/header.html");
-  const mainRes = await fetch("./components/main.html");
+  // const mainRes = await fetch("./components/main.html");
   const footerRes = await fetch("./components/footer.html");
-  if (!footerRes.ok && !mainRes.ok && !headerRes.ok) {
-    console.error("can't load header.html", res.status);
+
+  if (!footerRes.ok && !headerRes.ok) {
+    alert("can't load header.html", res.status);
     return;
   }
-  _header.innerHTML = await headerRes.text();
-  _main.innerHTML = await mainRes.text();
-  _footer.innerHTML = await footerRes.text();
+  return _header.innerHTML = await headerRes.text(), _footer.innerHTML = await footerRes.text();
 }
 
 if (document.readyState === "loading") {
